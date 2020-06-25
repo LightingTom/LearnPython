0234cb48aac409439e00862b0b134dc42049cf8c
Lenovo <1452442064@qq.com>
2020-06-25 17:10:26 +0800

    Learning Python

--- OOP.py
+++ OOP.py
@@ -5,7 +5,7 @@ from types import MethodType
 
 
 class foo1:
-    def __init__(self,var1):
+    def __init__(self, var1):
         self.var1 = var1
 
 
@@ -21,7 +21,7 @@ def foo1_method(self):
 
 
 # Bind the foo1_method with an instance a
-a.method = MethodType(foo1_method,a)
+a.method = MethodType(foo1_method, a)
 a.method()  # print
 b = foo1(2)
 # b.method()
@@ -29,18 +29,20 @@ b = foo1(2)
 
 # Bind 'foo1_method' with the class foo1, after binding, every instance of foo1 can use it
 foo1.foo1_method = foo1_method
-b.foo1_method() # print
+b.foo1_method()  # print
 
 
 # The keyword '__slots__' will restrict the attributes that foo2 have(including variables and methods)
 class foo2:
     __slots__ = 'var1'
 
-    def __init__(self,var1):
+    def __init__(self, var1):
         self.var1 = var1
 
 
 c = foo2(3)
+
+
 # it will report an error if bind other attributes(like shown below)
 # c.var2 = 4
 # c.method = foo1_method (also not allowed)
@@ -59,7 +61,7 @@ class foo3:
         return self._var1
 
     @var1.setter
-    def var1(self,val):
+    def var1(self, val):
         self._var1 = val
 
     @property
@@ -69,4 +71,77 @@ class foo3:
 
 d = foo3()
 d.var1 = 3
-print(format('var1: %d, var2: %d') % (d.var1,d.var2))
+print(format('var1: %d, var2: %d') % (d.var1, d.var2))
+
+
+# In python, there will be some custom class (定制类).
+# It is like implementation of some built-in method for the class Object in Java
+class Student:
+    def __init__(self, name, id, score):
+        self.name = name
+        self.id = id
+        self.score = score
+
+    # Like the toString() method
+    def __str__(self):
+        return format('Name: %s\nID: %s\nScore: %d') % (self.name, self.id, self.score)
+
+    # __repr__ is used in command line of python, shown to the developer, and __str__ is shown to the user
+    __repr__ = __str__
+
+
+print(Student('Tom', '11813015', 100))
+
+
+class Fib:
+    def __init__(self):
+        self.a, self.b = 0, 1
+
+# Used to implement the iterator
+    def __iter__(self):
+        return self
+
+# Used to implement the iterator
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        if self.a > 100000:
+            raise StopIteration()
+        return self.a
+
+# Implement the method get(i) like the list and the slice function
+    def __getitem__(self, n):
+        a, b = 1, 1
+        if isinstance(n, int):
+            for i in range(n):
+                a,b = b, a+b
+            return a
+        if isinstance(n,slice):
+            start = n.start
+            stop = n.stop
+            if start is None:
+                start = 0
+            a, b = 1, 1
+            L = []
+            for x in range(stop):
+                if x >= start:
+                    L.append(a)
+                a, b = b, a + b
+            return L
+
+
+print(Fib().__getitem__(5))
+print(Fib()[:5])
+
+class Integer:
+    def __init__(self, n):
+        if not isinstance(n,int):
+            raise TypeError('Required int')
+        self.n = n
+
+    # Once implement __call__, the instance can be called as a function
+    def __call__(self):
+        print('value:',self.n)
+
+
+e = Integer(2)
+e()
