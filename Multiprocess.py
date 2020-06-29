c0e70b70ac139170d2bb4f0900806a8bbc55b23a
Lenovo <1452442064@qq.com>
2020-06-29 09:32:43 +0800

    Learning Python

---
+++ Multiprocess.py
@@ -0,0 +1,96 @@
+# from multiprocessing import Process
+# import os
+#
+#
+# # 子进程要执行的代码
+# def run_proc(name):
+#     print('Run child process %s (%s)...' % (name, os.getpid()))
+#
+#
+# if __name__ == '__main__':
+#     print('Parent process %s.' % os.getpid())
+#     # About process, target represents the function to run in the sub-process,
+#     # args represents the augments in the function
+#     p = Process(target=run_proc, args=('test',))
+#     print('Child process will start.')
+#     p.start()
+#     p.join()
+#     print('Child process end.')
+
+
+# # Process Pool
+# # Can create many process in a simple way
+# from multiprocessing import Pool
+# import os, time, random
+#
+#
+# def long_time_task(name):
+#     print('Run task %s (%s)...' % (name, os.getpid()))
+#     start = time.time()
+#     time.sleep(random.random() * 3)
+#     end = time.time()
+#     print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+#
+#
+# # The augment given to Pool represents how many tasks can be processed at the same time
+# # Can try Pool(5) and Pool(4) for the following code, it will have different result
+# if __name__ == '__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Pool(5)
+#     for i in range(5):
+#         p.apply_async(long_time_task, args=(i,))
+#     print('Waiting for all subprocesses done...')
+#     p.close()
+#     p.join()
+#     print('All subprocesses done.')
+
+# import subprocess
+#
+# print('$ nslookup www.python.org')
+# r = subprocess.call(['nslookup', 'www.python.org'])
+# print('Exit code:', r)
+
+import subprocess
+
+print('$ nslookup')
+# Pipe is used for the communication between processes
+p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# p.communicate(args) args is the information passed to the subprocess
+output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+print(output.decode())
+print('Exit code:', p.returncode)
+
+from multiprocessing import Process, Queue
+import os, time, random
+
+
+# 写数据进程执行的代码:
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in ['A', 'B', 'C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+
+# 读数据进程执行的代码:
+def read(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+
+if __name__ == '__main__':
+    # 父进程创建Queue，并传给各个子进程：
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    # 启动子进程pw，写入:
+    pw.start()
+    # 启动子进程pr，读取:
+    pr.start()
+    # 等待pw结束:
+    pw.join()
+    # pr进程里是死循环，无法等待其结束，只能强行终止:
+    pr.terminate()
